f7715792e3a1cd6fbb4d051fb2f9e9393ca5f521
crashfalloutfan <aaronjacob104@gmail.com>
2024-09-28 20:58:37 -0400

    Finaly

--- Source/Convert.cpp
+++ Source/Convert.cpp
@@ -1,153 +1,151 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 #include <sstream>
-#include <map>
-#include <filesystem>
+#include <algorithm>
 
-
-std::string translateBatchToCpp(const std::string& batchLine) {
-    std::map<std::string, std::string> translations = {
-        {"assoc", "system(\"assoc\");"},
-        {"attrib", "system(\"attrib\");"},
-        {"break", "system(\"break\");"},
-        {"bcdedit", "system(\"bcdedit\");"},
-        {"cacls", "system(\"cacls\");"},
-        {"call", "system(\"call filename.bat\");"},
-        {"cd", "std::filesystem::current_path();"},
-        {"chcp", "system(\"chcp\");"},
-        {"chdir", "std::filesystem::current_path();"},
-        {"chkdsk", "system(\"chkdsk\");"},
-        {"chkntfs", "system(\"chkntfs\");"},
-        {"cls", "system(\"cls\");"},
-        {"cmd", "system(\"cmd\");"},
-        {"color", "system(\"color\");"},
-        {"comp", "system(\"comp file1 file2\");"},
-        {"compact", "system(\"compact\");"},
-        {"convert", "system(\"convert\");"},
-        {"copy", "system(\"copy source destination\");"},
-        {"date", "system(\"date\");"},
-        {"del", "std::remove(\"filename\");"},
-        {"dir", "system(\"dir\");"},
-        {"diskpart", "system(\"diskpart\");"},
-        {"doskey", "system(\"doskey\");"},
-        {"driverquery", "system(\"driverquery\");"},
-        {"echo", "std::cout << "},
-        {"endlocal", "system(\"endlocal\");"},
-        {"erase", "std::remove(\"filename\");"},
-        {"exit", "return 0;"},
-        {"fc", "system(\"fc file1 file2\");"},
-        {"find", "system(\"find string file\");"},
-        {"findstr", "system(\"findstr string file\");"},
-        {"for", "// Loop through files (use for loop in C++)"},
-        {"format", "system(\"format drive:\");"},
-        {"fsutil", "system(\"fsutil\");"},
-        {"ftype", "system(\"ftype\");"},
-        {"goto", "// Jump to label (not directly translatable)"},
-        {"gpresult", "system(\"gpresult\");"},
-        {"graftabl", "system(\"graftabl\");"},
-        {"help", "system(\"help\");"},
-        {"icacls", "system(\"icacls\");"},
-        {"if", "// Conditional statement (use if in C++)"},
-        {"label", "system(\"label\");"},
-        {"md", "std::filesystem::create_directory(\"directory_name\");"},
-        {"mkdir", "std::filesystem::create_directory(\"directory_name\");"},
-        {"mklink", "system(\"mklink link target\");"},
-        {"mode", "system(\"mode\");"},
-        {"more", "system(\"more file\");"},
-        {"move", "system(\"move source destination\");"},
-        {"openfiles", "system(\"openfiles\");"},
-        {"path", "system(\"path\");"},
-        {"pause", "system(\"pause\");"},
-        {"popd", "system(\"popd\");"},
-        {"print", "system(\"print filename\");"},
-        {"prompt", "system(\"prompt\");"},
-        {"pushd", "system(\"pushd directory\");"},
-        {"rd", "std::filesystem::remove(\"directory_name\");"},
-        {"recover", "system(\"recover\");"},
-        {"rem", "//"},
-        {"ren", "std::rename(\"oldname\", \"newname\");"},
-        {"rename", "std::rename(\"oldname\", \"newname\");"},
-        {"replace", "system(\"replace file1 file2\");"},
-        {"rmdir", "std::filesystem::remove(\"directory_name\");"},
-        {"robocopy", "system(\"robocopy\");"},
-        {"set", "std::string "},
-        {"setlocal", "system(\"setlocal\");"},
-        {"sc", "system(\"sc\");"},
-        {"schtasks", "system(\"schtasks\");"},
-        {"shift", "system(\"shift\");"},
-        {"shutdown", "system(\"shutdown\");"},
-        {"sort", "system(\"sort\");"},
-        {"start", "system(\"start command\");"},
-        {"subst", "system(\"subst\");"},
-        {"systeminfo", "system(\"systeminfo\");"},
-        {"tasklist", "system(\"tasklist\");"},
-        {"taskkill", "system(\"taskkill\");"},
-        {"time", "system(\"time\");"},
-        {"title", "system(\"title\");"},
-        {"tree", "system(\"tree\");"},
-        {"type", "std::ifstream file(\"filename\"); while (std::getline(file, line)) std::cout << line << std::endl;"},
-        {"ver", "system(\"ver\");"},
-        {"verify", "system(\"verify\");"},
-        {"vol", "system(\"vol\");"},
-        {"xcopy", "system(\"xcopy\");"},
-        {"wmic", "system(\"wmic\");"}
+std::string convertBatchToCpp(const std::string& line) {
+    std::unordered_map<std::string, std::string> commandMap = {
+        {"ASSOC", "system(\"assoc\");"},
+        {"ATTRIB", "system(\"attrib\");"},
+        {"BREAK", "system(\"break\");"},
+        {"BCDEDIT", "system(\"bcdedit\");"},
+        {"CACLS", "system(\"cacls\");"},
+        {"CALL", "system(\"call\");"},
+        {"CD", "system(\"cd\");"},
+        {"CHCP", "system(\"chcp\");"},
+        {"CHDIR", "system(\"chdir\");"},
+        {"CHKDSK", "system(\"chkdsk\");"},
+        {"CHKNTFS", "system(\"chkntfs\");"},
+        {"CLS", "system(\"cls\");"},
+        {"CMD", "system(\"cmd\");"},
+        {"COLOR", "system(\"color\");"},
+        {"COMP", "system(\"comp\");"},
+        {"COMPACT", "system(\"compact\");"},
+        {"CONVERT", "system(\"convert\");"},
+        {"COPY", "system(\"copy\");"},
+        {"DATE", "system(\"date\");"},
+        {"DEL", "system(\"del\");"},
+        {"DIR", "system(\"dir\");"},
+        {"DISKPART", "system(\"diskpart\");"},
+        {"DOSKEY", "system(\"doskey\");"},
+        {"DRIVERQUERY", "system(\"driverquery\");"},
+        {"ECHO", "std::cout << \"<message>\" << std::endl;"},
+        {"ENDLOCAL", "system(\"endlocal\");"},
+        {"ERASE", "system(\"erase\");"},
+        {"EXIT", "exit(0);"},
+        {"FC", "system(\"fc\");"},
+        {"FIND", "system(\"find\");"},
+        {"FINDSTR", "system(\"findstr\");"},
+        {"FOR", "system(\"for\");"},
+        {"FORMAT", "system(\"format\");"},
+        {"FSUTIL", "system(\"fsutil\");"},
+        {"FTYPE", "system(\"ftype\");"},
+        {"GOTO", "goto <label>;"},
+        {"GPRESULT", "system(\"gpresult\");"},
+        {"GRAFTABL", "system(\"graftabl\");"},
+        {"HELP", "system(\"help\");"},
+        {"ICACLS", "system(\"icacls\");"},
+        {"IF", "if (condition) { /* command */ }"},
+        {"LABEL", "system(\"label\");"},
+        {"MD", "system(\"md\");"},
+        {"MKDIR", "system(\"mkdir\");"},
+        {"MKLINK", "system(\"mklink\");"},
+        {"MODE", "system(\"mode\");"},
+        {"MORE", "system(\"more\");"},
+        {"MOVE", "system(\"move\");"},
+        {"OPENFILES", "system(\"openfiles\");"},
+        {"PATH", "system(\"path\");"},
+        {"PAUSE", "system(\"pause\");"},
+        {"POPD", "system(\"popd\");"},
+        {"PRINT", "system(\"print\");"},
+        {"PROMPT", "system(\"prompt\");"},
+        {"PUSHD", "system(\"pushd\");"},
+        {"RD", "system(\"rd\");"},
+        {"RECOVER", "system(\"recover\");"},
+        {"REM", "//"},
+        {"REN", "system(\"ren\");"},
+        {"RENAME", "system(\"rename\");"},
+        {"REPLACE", "system(\"replace\");"},
+        {"RMDIR", "system(\"rmdir\");"},
+        {"ROBOCOPY", "system(\"robocopy\");"},
+        {"SET", "system(\"set\");"},
+        {"SETLOCAL", "system(\"setlocal\");"},
+        {"SC", "system(\"sc\");"},
+        {"SCHTASKS", "system(\"schtasks\");"},
+        {"SHIFT", "system(\"shift\");"},
+        {"SHUTDOWN", "system(\"shutdown\");"},
+        {"SORT", "system(\"sort\");"},
+        {"START", "system(\"start <application>\");"},
+        {"SUBST", "system(\"subst\");"},
+        {"SYSTEMINFO", "system(\"systeminfo\");"},
+        {"TASKLIST", "system(\"tasklist\");"},
+        {"TASKKILL", "system(\"taskkill\");"},
+        {"TIME", "system(\"time\");"},
+        {"TITLE", "system(\"title\");"},
+        {"TREE", "system(\"tree\");"},
+        {"TYPE", "system(\"type\");"},
+        {"VER", "system(\"ver\");"},
+        {"VERIFY", "system(\"verify\");"},
+        {"VOL", "system(\"vol\");"},
+        {"XCOPY", "system(\"xcopy\");"},
+        {"WMIC", "system(\"wmic\");"},
     };
 
-    std::string cppLine;
-    std::istringstream iss(batchLine);
-    std::string command;
-    iss >> command;
+    std::string command = line;
 
-    if (translations.find(command) != translations.end()) {
-        cppLine = translations[command];
+    // Remove leading whitespace
+    command.erase(0, command.find_first_not_of(" \t"));
 
-        if (command == "echo") {
-            std::string message = batchLine.substr(5);
-            cppLine += "\"" + message + "\";\n";
-        } else if (command == "set") {
-            cppLine += batchLine.substr(4) + ";\n";
-        } else if (command == "md" || command == "mkdir") {
-            std::string dirName = batchLine.substr(3);
-            cppLine += "std::filesystem::create_directory(\"" + dirName + "\");\n";
-        } else if (command == "rd" || command == "rmdir") {
-            std::string dirName = batchLine.substr(3);
-            cppLine += "std::filesystem::remove(\"" + dirName + "\");\n";
-        } else {
-            cppLine += ";\n";
-        }
-    } else {
-        cppLine = "// Unrecognized command: " + batchLine + "\n";
+    // Remove any leading "@" from the command
+    if (command[0] == '@') {
+        command.erase(0, 1);
     }
 
-    return cppLine;
+    // Split the command to separate the command word from its arguments
+    std::istringstream iss(command);
+    std::string commandWord;
+    iss >> commandWord;
+
+    // Convert the command word to uppercase for case insensitive matching
+    std::transform(commandWord.begin(), commandWord.end(), commandWord.begin(), ::toupper);
+
+    if (commandMap.find(commandWord) != commandMap.end()) {
+        std::string args;
+        std::getline(iss, args); // Get the rest of the line (arguments)
+        return commandMap[commandWord] + args; // Append arguments if any
+    } else {
+        return "// Unknown command: " + line;
+    }
 }
 
 int main() {
     std::string filepath;
-
     std::cout << "Enter the filepath of the batch file: ";
     std::getline(std::cin, filepath);
 
-    if (!std::filesystem::exists(filepath)) {
-        std::cerr << "Error: File does not exist at path: " << filepath << std::endl;
-        return 1;
-    }
-
     std::ifstream batchFile(filepath);
-    if (!batchFile) {
-        std::cerr << "Error: Unable to open file at path: " << filepath << std::endl;
+    if (!batchFile.is_open()) {
+        std::cerr << "Error: Unable to open the batch file. Please check the file path." << std::endl;
+        std::cout << "Press Enter to exit...";
+        std::cin.ignore();
+        std::cin.get();
         return 1;
     }
 
-    std::string batchLine;
-    std::cout << "\nC++ Equivalent Code:\n";
+    std::cout << "C++ Equivalent Code:" << std::endl;
 
-    while (std::getline(batchFile, batchLine)) {
-        std::string cppLine = translateBatchToCpp(batchLine);
-        std::cout << cppLine;
+    std::string line;
+    while (std::getline(batchFile, line)) {
+        std::cout << convertBatchToCpp(line) << std::endl;
     }
 
     batchFile.close();
+    
+    std::cout << "Conversion completed." << std::endl;
+    std::cout << "Press Enter to exit...";
+    std::cin.ignore();
+    std::cin.get();
     return 0;
 }
